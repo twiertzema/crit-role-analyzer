e0d8d85114e717daa49cd7fc3fa2250709ef22f3
twiertzema <4044268+twiertzema@users.noreply.github.com>
2020-08-02 11:40:38 -0500

    Added a bunch more analysis

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from functools import reduce
 
 from constants import FIRST_EP, MY_CURRENT_EP
 
+MIKE_EP = "/wiki/Stalker_in_the_Swamp"
+
 
 def episodes_from(episodes, from_episode: str):
     from_index = 0
@@ -59,6 +61,11 @@ def parse_date(date_str: str):
             return datetime.strptime(date_str.split(" ")[0], "%m-%d-%Y")
 
 
+def time_str(time_in_seconds):
+    days, hours, minutes, seconds = break_down_time(time_in_seconds)
+    return f"{days}:{hours}:{minutes}:{seconds} ({time_in_seconds})"
+
+
 def get_avg_daily_runtime(episodes, total_runtime):
     first_date = parse_date(episodes[0]["airdate"])
     last_date = parse_date(episodes[-1]["airdate"])
@@ -68,20 +75,76 @@ def get_avg_daily_runtime(episodes, total_runtime):
 
 def main():
     episodes = load_episodes()
+    no_talks = list(filter(lambda ep: "Talks Machina" not in ep["title"], episodes))
 
     total_runtime = get_total_runtime(episodes)
-    days, hours, minutes, seconds = break_down_time(total_runtime)
-    print(f"Total runtime: {days}:{hours}:{minutes}:{seconds}")
     print(f"Gross total: {total_runtime}")
+    print(f"Total runtime: {time_str(total_runtime)}")
+
+    total_runtime_no_talks = get_total_runtime(no_talks)
+    print(f"Total runtime (no Talks): {time_str(total_runtime_no_talks)}")
+
+    remaining_episodes = episodes_from(episodes, MY_CURRENT_EP)
+    remaining_runtime = get_total_runtime(remaining_episodes)
+    print(f"Runtime remaining: {time_str(remaining_runtime)}")
+
+    remaining_no_talks_episodes = episodes_from(no_talks, MY_CURRENT_EP)
+    remaining_runtime_no_talks = get_total_runtime(remaining_no_talks_episodes)
+    print(f"Runtime remaining (no Talks): {time_str(remaining_runtime_no_talks)}")
+
     print()
+
+    print(
+        f"Average (main) episode length: {time_str(total_runtime_no_talks / len(no_talks))}"
+    )
 
     avg_runtime = get_avg_daily_runtime(episodes, total_runtime)
     print(f"Average runtime / week: {break_down_time(avg_runtime * 7)}")
 
-    avg_runtime = get_avg_daily_runtime(
+    avg_runtime_no_talks = get_avg_daily_runtime(
+        remaining_no_talks_episodes, remaining_runtime_no_talks
+    )
+    print(
+        f"Average runtime / week (no Talks): {break_down_time(avg_runtime_no_talks * 7)}"
+    )
+
+    recent_avg_runtime = get_avg_daily_runtime(
         episodes_from(episodes, "/wiki/Talks_Machina_Episode_1"), total_runtime
     )
-    print(f'Average runtime / week since "Talks": {break_down_time(avg_runtime * 7)}')
+    print(
+        f'Average runtime / week since "Talks": {break_down_time(recent_avg_runtime * 7)}'
+    )
+
+    print()
+
+    latest_airdate = parse_date(episodes[-1]["airdate"])
+    target_date = datetime(year=2020, month=12, day=31)
+    target_diff = (target_date - latest_airdate).days
+    print(f"Days remaining until target: {target_diff}")
+
+    print()
+
+    additional_runtime = int(target_diff * recent_avg_runtime)
+    total_target_runtime = remaining_runtime + additional_runtime
+    print(f"Total runtime until target: {time_str(total_target_runtime)}")
+
+    target_watch_rate = int(total_target_runtime / target_diff)
+    print(f"Target watch rate / week: {time_str(target_watch_rate * 7)}")
+
+    print()
+
+    additional_runtime_no_talks = int(target_diff * avg_runtime_no_talks)
+    total_target_runtime_no_talks = (
+        remaining_runtime_no_talks + additional_runtime_no_talks
+    )
+    print(
+        f"Total runtime until target (no Talks): {time_str(total_target_runtime_no_talks)}"
+    )
+
+    target_watch_rate_no_talks = int(total_target_runtime_no_talks / target_diff)
+    print(
+        f"Target watch rate / week (no Talks): {time_str(target_watch_rate_no_talks * 7)}"
+    )
 
 
 if __name__ == "__main__":
